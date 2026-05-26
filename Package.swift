52483d012bb2434cdc13bd2bba504b6b0f9d65d5
RNT56 <rnt56.2b@gmail.com>
2026-05-26 15:35:23 +0200

    Pin MLX runtime layout kernels

--- Package.swift
+++ Package.swift
@@ -44,7 +44,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/RNT56/mlx-swift",
-            revision: "c96dd8c7b374fa50d64b35bf8c5d7739df7d9984"),
+            revision: "5a990749fae2125f7a69506b486238ab1ac166c2"),
         .package(url: "https://github.com/swiftlang/swift-syntax.git", "600.0.0" ..< "604.0.0"),
     ],
     targets: [
